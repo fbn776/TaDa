4e2025803ee645ced5bd64aad78294b39199f2aa
fbn776 <fbn776@gmail.com>
2025-08-09 20:07:06 +0530

    fix: remove unwanted code

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,14 @@
 "use client"
-
-import {ShimmerButton} from "@/components/magicui/shimmer-button";
-
 import confetti from "canvas-confetti";
 
 
-
 export default function CelebrationPage() {
-    const celebrateConfetti = () => {
-        const duration = 5 * 1000;
-        const animationEnd = Date.now() + duration;
-        const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
-
-        const randomInRange = (min: number, max: number) =>
-            Math.random() * (max - min) + min;
-
-        const interval = window.setInterval(() => {
-            const timeLeft = animationEnd - Date.now();
-
-            if (timeLeft <= 0) {
-                return clearInterval(interval);
-            }
-
-            const particleCount = 200 * (timeLeft / duration);
-            confetti({
-                ...defaults,
-                particleCount,
-                origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
-            });
-
-            confetti({
-                ...defaults,
-                particleCount,
-                origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
-            });
-        }, 250);
-    };
-
     const handleTouch = (event: React.MouseEvent<HTMLDivElement>) => {
         confetti({
-            particleCount: 150,
+            // particleCount: 150,
             spread: 360,
-            origin: { x: event.clientX / window.innerWidth, y: event.clientY / window.innerHeight },
+            startVelocity: 20,
+            origin: {x: event.clientX / window.innerWidth, y: event.clientY / window.innerHeight},
         })
     }
 
@@ -50,14 +17,19 @@ export default function CelebrationPage() {
             className="min-h-screen bg flex items-center justify-center p-8"
             onClick={handleTouch}
         >
-            <ShimmerButton onClick={() => {
-                celebrateConfetti();
-            }}
-                           className="text-2xl px-10"
-                           background="#5259f7"
-            >
-                Celebrate
-            </ShimmerButton>
+
+            <div className="text-[#5259f7] text-2xl font-bold text-center opacity-20">
+                Click Anywhere to Celebrate!
+            </div>
+
+            {/*<ShimmerButton onClick={() => {*/}
+            {/*    celebrateConfetti();*/}
+            {/*}}*/}
+            {/*               className="text-2xl px-10"*/}
+            {/*               background="#5259f7"*/}
+            {/*>*/}
+            {/*    Click Anywhere to Celebrate!*/}
+            {/*</ShimmerButton>*/}
         </div>
     )
 }
